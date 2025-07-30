5dcd32d8c659f141b6a6c24a9215b77929c076ec
vaijayanth sheri <132249172+vaijayanth-sheri@users.noreply.github.com>
2025-07-30 19:01:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 # exit on error
 set -o errexit
 
-# --- System Dependencies for WeasyPrint ---
 apt-get update
 apt-get install -y libpango-1.0-0 libharfbuzz0 libpangoft2-1.0-0
-
-# --- Python Dependencies ---
-pip install --upgrade pip
-pip install -r requirements.txt
